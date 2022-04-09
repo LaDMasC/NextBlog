a67894a3c8fb0a23296167ea1790cd802325d925
차상원 <cktkddnjs1@icloud.com>
2022-04-09 13:52:12 +0900

    수정: React 18에서 발생하는 Nextjs 문제 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,9 +5,18 @@ import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import { TopNav } from "src/compoenets";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
+
+  const [showChild, setShowChild] = useState(false);
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
+  if (!showChild) return <div>Loading...</div>;
+
   return (
     <RecoilRoot>
       <TopNav url={router.pathname} />
